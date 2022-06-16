0074bf4ed0c21b058c4df4f031e430b0362decca
Mo7amedSMahdi <Mo7ammed.salah@outlook.com>
2022-06-16 17:47:49 +0300

    print the result and fix linters

--- my_list.rb
+++ my_list.rb
@@ -1,23 +1,30 @@
 require_relative './enumerable'
 
 class MyList
-    include Enumerable
-    def initialize(*args)
-        @list = args
+  include Enumerable
+  def initialize(*args)
+    @list = args
+  end
+
+  def each()
+    return to_enum(:each) unless block_given?
+
+    counter = 0
+    while counter < @list.size
+      yield @list[counter]
+      counter += 1
     end
 
-    def each()
-        return to_enum(:each) unless block_given?
-        counter = 0 
-        while counter < @list.size
-            yield @list[counter]
-      counter += 1
-        end
-        @list
-    end
+    @list
+  end
 end
+
 list = MyList.new(1, 2, 3, 4)
- list.all? {|e| e > 5}
-list.any? {|e| e == 2}
- list.any? {|e| e == 5}
-list.filter {|e| e.even?}
+puts(list.all? { |e| e > 5 })
+# => false
+puts(list.any? { |e| e == 2 })
+# => true
+puts(list.any? { |e| e == 5 })
+# => false
+print(list.filter(&:even?))
+# => [2,4]
